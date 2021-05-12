010c742504a68e79308890a45c9f52dbda57cd87
yeraygar <yeray1dam@gmail.com>
2021-05-12 15:41:38 +0200

    Añado la clase Jugador

---
+++ Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatallaNaval
+{
+    public class Jugador
+    {
+        private int numero_disparos;
+
+        public Jugador(int numero_disparos)
+        {
+            this.numero_disparos = 10;
+        }
+
+        public int getNumero_disparos()
+        {
+            return numero_disparos;
+        }
+
+        //public 
+    }
+}
