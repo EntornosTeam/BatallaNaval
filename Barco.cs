02e96dc40e3201d6868ab0e8fadb58318e4c1a4e
samugp <samuelgp77@gmail.com>
2021-05-15 12:01:58 +0200

    Añadida clase Barco

---
+++ Barco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatallaNaval
+{
+    class Barco
+    {
+        private int size;
+        private int id = 0;
+        public static int maxID = -1;
+        //private string orientacion;
+
+        public Barco(int s)
+        {
+            this.size = s;
+            this.id = ++maxID;
+        }
+    }
+}
