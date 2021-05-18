3993f6f95f821fd500fc5bf694f4e6d9e723e8b0
Djessy Czaplicki <djessyczaplicki@gmail.com>
2021-05-18 14:19:35 +0200

    Actualización de los eventos click en los PictureBox

--- Juego.cs
+++ Juego.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,10 +19,44 @@ namespace BatallaNaval
 
         public Juego(TableLayoutPanel tabla, Tablero tablero, List<Barco> barcos)
         {
-            this.Controls.Add(tabla);
+            Controls.Add(tabla);
+            foreach (Control c in tabla.Controls)
+            {
+                PictureBox pb = c as PictureBox;
+                if (pb != null)
+                {
+                    RemoveClickEvent(pb);
+                    pb.Click += new System.EventHandler(pictureBox1_Click);
+                }
+            }
+
+
             this.tablero = tablero;
             this.barcos = barcos;
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Crea una lista con los eventos clicks del Picturebox enviado, y los elimina.
+        /// </summary>
+        /// <param name="pb">El PictureBox del que queremos que se eliminen los eventos click</param>
+        private void RemoveClickEvent(PictureBox pb)
+        {
+            FieldInfo f1 = typeof(Control).GetField("EventClick",
+                BindingFlags.Static | BindingFlags.NonPublic);
+
+            object obj = f1.GetValue(pb);
+            PropertyInfo pi = pb.GetType().GetProperty("Events",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            EventHandlerList list = (EventHandlerList)pi.GetValue(pb, null);
+            list.RemoveHandler(obj, list[obj]);
+        }
+
+
+        private void pictureBox1_Click(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Funciona jeje");
         }
     }
 }
